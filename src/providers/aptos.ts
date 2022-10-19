69d138f6a96792807c18bf02397fa63c4ae31ddb
mordochi <lynn@portto.com>
2022-10-19 16:59:45 +0800

    [Feature] Require appId to interact with the SDK

--- src/providers/aptos.ts
+++ src/providers/aptos.ts
@@ -40,6 +40,8 @@ export default class AptosProvider extends BloctoProvider implements AptosProvid
   constructor({ chainId, server, appId }: AptosProviderConfig) {
     super();
     invariant(chainId, "'chainId' is required");
+    invariant(appId, 'It is necessary to interact with Blocto wallet via your app id.');
+
     this.chainId = chainId;
     this.networkName = APT_CHAIN_ID_NAME_MAPPING[chainId];
     this.api = APT_CHAIN_ID_RPC_MAPPING[chainId];
@@ -228,7 +230,7 @@ export default class AptosProvider extends BloctoProvider implements AptosProvid
       }
 
       const location = encodeURIComponent(window.location.origin);
-      const loginFrame = createFrame(`${this.server}/authn?l6n=${location}&chain=aptos`);
+      const loginFrame = createFrame(`${this.server}/authn?l6n=${location}&chain=aptos&appId=${this.appId}`);
 
       attachFrame(loginFrame);
 
