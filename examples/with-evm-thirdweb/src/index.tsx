8bb2a7b55af1b367b49ada735aa1bf5493964375
calvinchang <q20274982@gmail.com>
2023-07-31 18:04:05 +0800

    docs: add thirdweb connector example

---
+++ examples/with-evm-thirdweb/src/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {
+  ThirdwebProvider,
+  bloctoWallet,
+  metamaskWallet,
+} from '@thirdweb-dev/react';
+import './styles/globals.css';
+
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+const activeChain = 'ethereum';
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
+  <React.StrictMode>
+    <ThirdwebProvider
+      activeChain={activeChain}
+      supportedWallets={[bloctoWallet(), metamaskWallet()]}
+    >
+      <App />
+    </ThirdwebProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
