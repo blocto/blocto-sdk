4b481f5a1f2e834e5aab6a35bb460d01a8796e92
calvinchang <q20274982@gmail.com>
2023-09-11 11:19:38 +0800

    fix: fix web3modal-next demo resolve error

--- examples/with-evm-web3modal-next/next.config.js
+++ examples/with-evm-web3modal-next/next.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
     config.resolve.fallback = { fs: false, net: false, tls: false }
     return config
   },
