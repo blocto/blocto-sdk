cb408b21313ea64d851550e9ddd2e8b734344cb0
calvinchang <q20274982@gmail.com>
2023-11-21 17:57:25 +0800

    feat: remove examples
    examples folder has been moved to blocto/blocto-sdk-examples repo

--- examples/with-evm-blocto-verify-sign/src/services/ethereum.js
+++
@@ -1,9 +0,0 @@
-import Web3 from "web3";
-import DappAuth from "@blocto/dappauth";
-
-const rpc = "https://rpc.ankr.com/polygon_mumbai";
-
-const web3 = new Web3(rpc);
-const dappAuth = new DappAuth(new Web3.providers.HttpProvider(rpc));
-
-export { web3, dappAuth };
