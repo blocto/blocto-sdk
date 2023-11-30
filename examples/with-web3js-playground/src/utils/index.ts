c8b4064e04c1ff3c63f44bbd5ba45b5c23ddb71b
calvinchang <q20274982@gmail.com>
2023-11-30 12:27:50 +0800

    refactor: remove example folder

--- examples/with-web3js-playground/src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-import generateUserOpHash from "./generateUserOpHash";
-import web3GenerateUserOpHash from "./web3GenerateUserOpHash";
-import sendUserOperation from "./sendUserOperation";
-
-export { generateUserOpHash, web3GenerateUserOpHash, sendUserOperation };
