b144c59c42b8906a66eea04372a0fab49b42d90d
sanyu1225 <pan831225@gmail.com>
2023-12-15 03:34:06 +0800

    fix: build and test

--- packages/dappauth/test/test-utils.js
+++ packages/dappauth/test/test-utils.js
@@ -2,48 +2,52 @@ import { randomBytes } from 'crypto';
 import { Buffer } from 'safe-buffer';
 import { keccak, ecsign, toRpcSig, hashPersonalMessage, toBuffer, stripHexPrefix, bufferToHex, privateToAddress } from 'ethereumjs-util';
 
-// @param {Buffer/String} message
-// @param {String} key the signing key
-// @return {String} the address of the wallet (identity)
-function signERC1654PersonalMessage(message, key, address) {
-  // we hash once before erc191MessageHash as it will be transmitted to Ethereum nodes and potentially logged
-  const messageHash = erc191MessageHash(keccak(message), address);
-  const signature = ecsign(messageHash, key);
-  return toRpcSig(signature.v, signature.r, signature.s);
+export function stringToHex(str) {
+  return bufferToHex(Buffer.from(str, 'utf8'));
 }
 
-// emulates what EOA wallets like MetaMask perform
-function signEOAPersonalMessage(message, key) {
-  const messageHash = hashPersonalMessage(toBuffer(message));
-  const signature = ecsign(messageHash, key);
-  return toRpcSig(signature.v, signature.r, signature.s);
-}
-
-// @param {Buffer} message
-// @param {String} address
-// @return {Buffer}
-function erc191MessageHash(message, address) {
+/**
+ * @param {Buffer} message 
+ * @param {String} address 
+ * @returns {Buffer}
+ */
+export function generateErc191MessageHash(message, address) {
   const erc191Message = Buffer.concat([
     Buffer.from('19', 'hex'),
     Buffer.from('00', 'hex'),
     Buffer.from(stripHexPrefix(address), 'hex'),
     message,
   ]);
-  return keccak(erc191Message);
+  const r = keccak(erc191Message)
+  return r;
 }
 
-function generateRandomKey() {
+
+/**
+ * @param {Buffer | String} message 
+ * @param {String} key 
+ * @param {String} address 
+ * @returns {String} the address of the wallet (identity)
+ */
+export function signERC1654PersonalMessage(message, key, address) {
+  // we hash once before erc191MessageHash as it will be transmitted to Ethereum nodes and potentially logged
+  const messageHash = generateErc191MessageHash(keccak(toBuffer(stringToHex(message))), address);
+  const signature = ecsign(messageHash, key);
+  return toRpcSig(signature.v, signature.r, signature.s);
+}
+
+// emulates what EOA wallets like MetaMask perform
+export function signEOAPersonalMessage(message, key) {
+  const messageHash = hashPersonalMessage(toBuffer(stringToHex(message)));
+  const signature = ecsign(messageHash, key);
+  return toRpcSig(signature.v, signature.r, signature.s);
+}
+
+
+export function generateRandomKey() {
   return toBuffer(`0x${randomBytes(32).toString('hex')}`);
 }
 
-function keyToAddress(key) {
+export function keyToAddress(key) {
   return bufferToHex(privateToAddress(key));
 }
-
-export default {
-  signERC1654PersonalMessage,
-  signEOAPersonalMessage,
-  erc191MessageHash,
-  generateRandomKey,
-  keyToAddress,
-};
